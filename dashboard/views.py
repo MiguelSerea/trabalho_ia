49e18b151baab5d19e0662db9ee99e3a75c83cba
Miguel Serea <100145693+MiguelSerea@users.noreply.github.com>
2024-11-14 19:34:17 -0300

    retirada do recebimento da chave status

--- dashboard/views.py
+++ dashboard/views.py
@@ -11,34 +11,29 @@ def process_request(request):
             data = json.loads(request.body)
             print("Dados recebidos:", data)  # Debug
 
-            # Extraia o status do JSON e verifique
-            status = data.get('status', 'failed')
-            print("Status recebido:", status)  # Debug
-
-            # Verifica se o status é 'failed', caso sim, retorna imediatamente
-            if status == 'failed':
-                print("Status é 'failed', respondendo sem cálculo")  # Debug
-                return JsonResponse({
-                    "status": "failed",
-                    "valor_potassio_hectare": 0.0,
-                    "valor_fosforo_hectare": 0.0
-                })
-
             # Extrai e converte os valores necessários para os cálculos
             CTC_ph7 = float(data.get('CTC_ph7', 0.0))
             argila = float(data.get('argila', 0.0))
             P = float(data.get('P', 0.0))
             K = float(data.get('K', 0.0))
             
-            
+            # Verifica se algum valor foi passado incorretamente
+            if any(v is None or v == 0.0 for v in [CTC_ph7, argila, P, K]):
+                print("Erro nos valores recebidos.")  # Debug
+                return JsonResponse({
+                    "status": "failed",
+                    "valor_potassio_hectare": 0.0,
+                    "valor_fosforo_hectare": 0.0
+                }, status=400)
+
             # Calcula os valores usando as funções utilitárias
             valor_potassio_hectare = fuzzy_potassio(K, CTC_ph7)
             valor_fosforo_hectare = fuzzy_fosforo(P, argila)
             print("Resultado cálculos:", valor_potassio_hectare, valor_fosforo_hectare)  # Debug
 
-            # Retorna a resposta JSON com os valores calculados
+            # Retorna a resposta JSON com os valores calculados e o status de sucesso
             return JsonResponse({
-                "status": status,
+                "status": "success",
                 "valor_potassio_hectare": valor_potassio_hectare,
                 "valor_fosforo_hectare": valor_fosforo_hectare
             })
